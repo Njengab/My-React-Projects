1bec81170d8355c2a1ab909d539b13bc6db23c22
BERNARD NJENGA MWAURA <87819077+Njengab@users.noreply.github.com>
2022-11-30 19:06:41 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div class="main">
+      <nav class="navigation">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Team</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
